827c02355115646aebc6870acc8362e4372f1af4
Kazamaki76 <140473875+Kazamaki76@users.noreply.github.com>
2023-10-25 15:39:42 +0700

    3 done

--- 03-post-app-async(redux-only)/_start/post-thunk-middleware.js
+++ 03-post-app-async(redux-only)/_start/post-thunk-middleware.js
@@ -1,24 +1,64 @@
 const { createStore, applyMiddleware } = require("redux");
+const axios = require("axios");
 const loggerMiddleware = require("redux-logger").default;
+const thunk = require("redux-thunk").default;
 // initialState
 const initialState = {
   posts: [],
+  loading: false,
+  error: "",
 };
 // Action
 const FETCH_POST_PENDING = "FETCH_POST_PENDING";
 const FETCH_POST_SUCCESS = "FETCH_POST_SUCCESS";
 const FETCH_POST_ERROR = "FETCH_POST_ERROR";
 
-const fetchPostPending = () => {
+// Action Creator
+const fetchPostPendingAction = () => {
   return {
     type: FETCH_POST_PENDING,
   };
 };
+
+const fetchPostSuccessAction = (posts) => {
+  return {
+    type: FETCH_POST_SUCCESS,
+    payload: posts,
+  };
+};
+
+const fetchPostErrorAction = (error) => {
+  return {
+    type: FETCH_POST_ERROR,
+    payload: error,
+  };
+};
+// Real Action  ****
+
+const fetchPostAction = () => {
+  return async (dispatch) => {
+    dispatch(fetchPostPendingAction());
+    try {
+      // make HTTP Request
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      dispatch(fetchPostSuccessAction(response.data));
+    } catch (error) {
+      dispatch(fetchPostErrorAction(error.message));
+    }
+  };
+};
+
 // Reducer
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_POST_PENDING:
-      return { posts: "HTML" };
+      return { ...state, loading: true };
+    case FETCH_POST_SUCCESS:
+      return { ...state, posts: action.payload, error: "", loading: false };
+    case FETCH_POST_ERROR:
+      return { ...state, posts: [], error: action.payload, loading: false };
     default:
       return state;
   }
@@ -34,13 +74,8 @@ const myMiddleware = () => {
   };
 };
 
-const rootMiddleware = applyMiddleware(loggerMiddleware, myMiddleware, myMiddleware);
+const rootMiddleware = applyMiddleware(loggerMiddleware, thunk);
 // Store
 const store = createStore(postReducer, rootMiddleware);
 
-// store.subscribe(() => {
-//     const data = store.getState();
-//     console.log(data);
-// });
-
-store.dispatch(fetchPostPending());
+store.dispatch(fetchPostAction());
